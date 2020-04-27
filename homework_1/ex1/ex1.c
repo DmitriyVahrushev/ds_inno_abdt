1393c82dcd4a9adde629d19bf49a48fd154cb3b0
DmitriyVahrushev <notmybestmail@yandex.ru>
2020-04-27 23:52:07 +0300

    Change ex1 and ex2

--- homework_1/ex1/ex1.c
+++ homework_1/ex1/ex1.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// На первой итерации создается копия текущего процесса (всего 2)
+// На следующей итерациии каждый процесс создаёт свою копию, процессов становится 4
+// Далее их будет 8, 16 и 32
 void main(){
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < 5; i++){
         fork();
         sleep(5);
     }
-    // int pid = fork();
-    // printf("Hello world!\n");
-    // if (pid < 0){
-    //     printf("Something went wrong\n");
-    // } else if (pid > 0){
-    //     printf("Hello from parent\n");
-    //     sleep(10);
-    // } else
-    // {
-    //     printf("Hello from child\n");
-    //     sleep(10);
-    // }
 }
